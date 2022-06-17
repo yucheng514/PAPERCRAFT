363ff2adca666fa37f899a58ab09cce0c9a9212f
daluozha <561890199@qq.com>
2022-06-17 15:48:16 +0800

    chore: fix mock & store

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,12 +1,10 @@
-let defaultColor = '#fff';
-let defaultImage = 'linear-gradient(to top right, #ccc 25%, transparent 25%, transparent 75%, #ccc 75%, #ccc), linear-gradient(to top right, #ccc 25%, transparent 25%, transparent 75%, #ccc 75%, #ccc)'
+let defaultColor = "#fff";
+let defaultImage =
+    "linear-gradient(to top right, #ccc 25%, transparent 25%, transparent 75%, #ccc 75%, #ccc), linear-gradient(to top right, #ccc 25%, transparent 25%, transparent 75%, #ccc 75%, #ccc)";
 
-import { defineStore } from 'pinia'
+import { defineStore } from "pinia";
 
-
-
-
-export const useStore = defineStore('main', {
+export const useStore = defineStore("main", {
     state: () => {
         return {
             viewerSize: 1,
@@ -15,25 +13,28 @@ export const useStore = defineStore('main', {
                 backgroundImage: defaultImage,
                 opacity: 1,
             },
-            curEl: {
-                lineHeight: 1,
-                letterSpacing: 0,
-                zIndex: 50,
-                digit: {
-                    integral: {
-                        place: 3,
-                        size: 24
-                    },
-                    decimal: {
-                        place: 2,
-                        size: 24
-                    }
-                }
-            },
-            count: 0
-        }
+            currentElement: null,
+            count: 0,
+            mouseDownEvent: null,
+        };
     },
     actions: {
-        setViewerSize(num: number) { this.viewerSize = num }
-    }
-})
+        setViewerSize(num: number) {
+            this.viewerSize = num;
+        },
+        setCurrent(val: any) {
+            this.currentElement = val;
+        },
+        setMouseDownEvent(val: any) {
+            this.mouseDownEvent = val;
+        },
+        clearMouseDownEvent() {
+            this.mouseDownEvent = null;
+        },
+        // setMouseDownEventCallback(val:any){
+        //     if(this.mouseDownEvent){
+        //         this.mouseDownEvent.callback(e)
+        //     }
+        // }
+    },
+});
