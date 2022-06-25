66c3cdd438bf4f12dccf995c304cdd38b022389a
daluozha <561890199@qq.com>
2022-06-26 04:51:37 +0800

    feat: tool kit

--- src/App.vue
+++ src/App.vue
@@ -3,25 +3,23 @@ import { Top, Left, Right, Main } from "./components/layouts";
 // import { useStore } from "@/store/store";
 // import { storeToRefs } from "pinia";
 // import { defineComponent } from "vue";
-import { NButton } from "naive-ui";
 
 // const store = useStore();
 // const { curEl, mainBg, count } = storeToRefs(store);
 </script>
 
 <template>
-    <!-- <n-button>naive-ui</n-button> -->
     <div class="flex flex-col justify-between h-full">
         <div class="flex-grow-0">
             <Top></Top>
         </div>
-        <div class="ml2 mr2 flex justify-between flex-grow">
+        <div class="flex justify-between flex-grow">
             <Left class=""></Left>
 
             <Main class=""></Main>
             <Right class=""></Right>
         </div>
-        <div class="m2 flex-grow-0 text-center text-sm color-#555">
+        <div class="p2 flex-grow-0 text-center text-sm color-#555 border">
             luozha © 2022
         </div>
     </div>
--- src/components/layouts/Right.vue
+++ src/components/layouts/Right.vue
@@ -1,10 +1,86 @@
 <template>
-  <div class="bg-red w100px h-full flex-grow-0">
-    工具栏
-  </div>
+    <div class="bg-white w400px h-full flex-grow-0 p15px">
+        工具栏
+        <div id="text-tool" v-if="currentElementKey">
+            <el-input
+                v-model="currentElement.content"
+                size="large"
+                placeholder="Please input"
+            />
+            <div>
+                行间距
+                <el-slider
+                    v-model="currentElement.lineHeight"
+                    size="small"
+                    :min="0"
+                    :max="50"
+                />
+            </div>
+            <div>
+                字间距
+                <el-slider
+                    v-model="currentElement.letterSpacing"
+                    size="small"
+                    :min="-50"
+                    :max="50"
+                />
+            </div>
+            <div>
+                透明度
+                <el-slider v-model="opacity" size="small" />
+            </div>
+            <div>
+                角度
+                <el-slider
+                    v-model="currentElement.transform"
+                    size="small"
+                    :min="0"
+                    :max="360"
+                />
+            </div>
+        </div>
+    </div>
 </template>
 
-<script setup lang='ts'>
+<script setup lang="ts">
+import { ref, reactive, computed, watch, onMounted } from "vue";
+import { useStore } from "@/store/store";
+import { storeToRefs } from "pinia";
+
+const store = useStore();
+const { viewerSize, currentElementKey, mouseDownEvent, allData } =
+    storeToRefs(store);
+const {
+    setViewerSize,
+    setCurrentElementKey,
+    setMouseDownEvent,
+    clearMouseDownEvent,
+    clearCurrentElement,
+    // setElement,
+} = store;
+
+const currentElement = computed(() => {
+    let curEl = {} as TextInter;
+    allData.value.some((el) => {
+        if (el.virtualKey === currentElementKey.value) {
+            curEl = el;
+            return true;
+        } else return false;
+    });
+    return curEl;
+});
+
+const opacity = computed({
+    get() {
+        return currentElement.value.opacity * 100;
+    },
+    set(val) {
+        currentElement.value.opacity = val / 100;
+    },
+});
 </script>
 <style>
+    #text-tool > div{
+        padding-top:10px ;
+    }
 </style>
