30dff49e56ed1bd3bcefc91627a14113499a05a3
daluozha <561890199@qq.com>
2022-07-04 20:20:35 +0800

    chore: update deps

--- vite.config.ts
+++ vite.config.ts
@@ -1,22 +1,34 @@
-import { fileURLToPath, URL } from 'url'
+import { fileURLToPath, URL } from "url";
 
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import Unocss from 'unocss/vite'
+// import { defineConfig } from "vite";
+import { defineConfig } from "vitest/config";
+
+import vue from "@vitejs/plugin-vue";
+import Unocss from "unocss/vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [vue(), Unocss({
-        // presets: [],
-        rules: [['m-1', { margin: '0.25rem' }],]
-    })],
+    plugins: [
+        vue(),
+        Unocss({
+            // presets: [],
+            rules: [["m-1", { margin: "0.25rem" }]],
+        }),
+    ],
     resolve: {
         alias: {
-            '@': fileURLToPath(new URL('./src', import.meta.url))
-        }
+            "@": fileURLToPath(new URL("./src", import.meta.url)),
+        },
     },
-    base: './',
+    base: "./",
     server: {
-        open: true
-    }
-})
+        open: true,
+    },
+    test: {
+        // enable jest-like global test APIs
+        globals: true,
+        // simulate DOM with happy-dom
+        // (requires installing happy-dom as a peer dependency)
+        environment: "happy-dom",
+    },
+});
