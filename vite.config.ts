472e78f2624f7f9488159b4b4d38e1936aac0dce
daluozha <561890199@qq.com>
2022-07-06 15:39:32 +0800

    chore: add deps

--- vite.config.ts
+++ vite.config.ts
@@ -1,22 +1,35 @@
-import { fileURLToPath, URL } from 'url'
+import { fileURLToPath, URL } from "url";
 
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import Unocss from 'unocss/vite'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import Unocss from "unocss/vite";
+import presetIcons from "@unocss/preset-icons";
+import { presetUno } from "unocss";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [vue(), Unocss({
-        // presets: [],
-        rules: [['m-1', { margin: '0.25rem' }],]
-    })],
+    plugins: [
+        vue(),
+        Unocss({
+            presets: [
+                presetIcons({
+                    prefix: "i-",
+                    extraProperties: {
+                        display: "inline-block",
+                    },
+                }),
+                presetUno(),
+            ],
+            // rules: [["m-1", { margin: "0.25rem" }]],
+        }),
+    ],
     resolve: {
         alias: {
-            '@': fileURLToPath(new URL('./src', import.meta.url))
-        }
+            "@": fileURLToPath(new URL("./src", import.meta.url)),
+        },
     },
-    base: './',
+    base: "./",
     server: {
-        open: true
-    }
-})
+        open: true,
+    },
+});
