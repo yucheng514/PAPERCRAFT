e0b50e5f4ec3b38b06d000ec67277e7a73707295
daluozha <561890199@qq.com>
2022-07-10 00:05:00 +0800

    style: top bar

--- src/components/layouts/Top.vue
+++ src/components/layouts/Top.vue
@@ -1,15 +1,34 @@
 <template>
-    <div class="h50px flex justify-between bg-[#f7faf7] p10px">
+    <div
+        id="TopBar"
+        class="h50px flex justify-between bg-[#f7faf7] p10px items-center"
+    >
         <div>PAPERCRAFT</div>
-        <el-link href="https://github.com/daluozha/PAPERCRAFT" target="_blank"
+        <!-- <el-link href="https://github.com/daluozha/PAPERCRAFT" target="_blank"
             >github</el-link
+        > -->
+        <a
+            href="https://github.com/daluozha/PAPERCRAFT"
+            target="_blank"
+            title="GitHub"
         >
+            <div class="i-uil-github-alt color-#828891 text-6"></div>
+        </a>
         <!-- <div>撤销，重做</div> -->
         <!-- <div>保存</div> -->
     </div>
 </template>
 
 <script setup lang="ts">
-import { ElLink } from "element-plus";
+// import { ElLink } from "element-plus";
 </script>
-<style></style>
+<style>
+/* #TopBar > a {
+    cursor: pointer;
+    text-decoration: none;
+    color: inherit;
+    transition: opacity 0.2s ease;
+    opacity: 0.6;
+    outline: none;
+} */
+</style>
